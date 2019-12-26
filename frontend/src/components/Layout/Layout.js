66b498da212968b82864d28fd947b681dcaa7ce3
Bui Kiem <buivantrung1105@gmail.com>
2019-12-26 22:11:05 +0700

    Add 'Navbar' component to layout

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Navbar } from "../Navbar/Navbar";
 
 export const Layout = ({ children }) => {
   return (
     <>
-      <header>Header</header>
+      <header>
+        <Navbar />
+      </header>
       <main>{children}</main>
       <footer>Footer</footer>
     </>

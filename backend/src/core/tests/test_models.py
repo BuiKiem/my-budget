3a6563cbdf41fdcf37202dc0d1b70a28d754f2f4
Bui Kiem <buivantrung1105@gmail.com>
2019-12-28 20:54:33 +0700

    Add unitest for 'accounts' endpoints

--- backend/src/core/tests/test_models.py
+++ backend/src/core/tests/test_models.py
@@ -4,9 +4,10 @@ from core.models import Account
 
 
 class AccountTestCase(TestCase):
-
     def test_string_representation(self) -> None:
         """Ensure that the model use 'name' for string representation."""
-        account = Account(name="Cash", color=Account.ColorChoices.GREEN, initial_balance=1000)
+        account = Account(
+            name="Cash", color=Account.ColorChoices.GREEN, initial_balance=1000
+        )
 
         self.assertEquals(str(account), "Cash")

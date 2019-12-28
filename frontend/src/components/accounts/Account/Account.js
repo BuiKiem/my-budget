fe50fe138f1d74b4f462ebd4221302e6bb4062b7
Bui Kiem <buivantrung1105@gmail.com>
2019-12-29 00:20:51 +0700

    Display 'Account' with data

---
+++ frontend/src/components/accounts/Account/Account.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+export const Account = ({ account: { name, balance, color } }) => {
+  return (
+    <Button variant="contained" color="primary">
+      <h3>{name}</h3>
+      <h4>{balance}</h4>
+    </Button>
+  );
+};
--- frontend/src/components/accounts/AccountList/AccountList.js
+++ frontend/src/components/accounts/AccountList/AccountList.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { Card } from "@material-ui/core";
+import { Account } from "../Account/Account";
 
 export const AccountList = () => {
-  return <h1>Account List</h1>;
+  const accounts = [
+    {
+      id: 1,
+      name: "cash",
+      balance: 10000,
+      color: "607d8b"
+    },
+    {
+      id: 2,
+      name: "saving",
+      balance: 20000,
+      color: "ff9800"
+    }
+  ];
+
+  return (
+    <Card>
+      <h2>List of accounts</h2>
+      {accounts.map(account => (
+        <Account key={account.id} account={account} />
+      ))}
+    </Card>
+  );
 };

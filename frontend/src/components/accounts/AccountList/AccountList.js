b2e56069eb96ff62865c4f765439edc40f3d64c1
Bui Kiem <buivantrung1105@gmail.com>
2019-12-29 13:46:20 +0700

    Grid display for the list

--- frontend/src/components/accounts/AccountList/AccountList.js
+++ frontend/src/components/accounts/AccountList/AccountList.js
@@ -6,7 +6,9 @@ import {
   CardHeader,
   CardContent,
   Typography,
-  Grid
+  Grid,
+  makeStyles,
+  useTheme
 } from "@material-ui/core";
 import {
   AddCircle as AddCircleIcon,
@@ -15,7 +17,19 @@ import {
 
 import { Account } from "../Account/Account";
 
+const useStyles = makeStyles({
+  root: {
+    height: "100%",
+    width: "100%",
+    color: props => props.infoColor.main,
+    borderColor: props => props.infoColor.main
+  }
+});
+
 export const AccountList = () => {
+  const theme = useTheme();
+  const infoColor = theme.palette.info;
+  const classes = useStyles({ infoColor });
   const accounts = [
     {
       id: 1,
@@ -32,41 +46,47 @@ export const AccountList = () => {
   ];
 
   return (
-    <section aria-label="accounts section">
-      <Card>
-        <CardHeader
-          title={
-            <Typography variant="h5" component="h2">
-              List of accounts
-            </Typography>
-          }
-          action={
-            <IconButton aria-label="configure account">
-              <SettingsIcon />
-            </IconButton>
-          }
-        />
-        <CardContent>
-          <Grid container spacing={1}>
-            {accounts.map(account => (
-              <Grid item xs={6} key={account.id}>
-                <Account account={account} />
-              </Grid>
-            ))}
-            <Grid item xs={6}>
-              <Button
-                variant="outlined"
-                size="small"
-                endIcon={<AddCircleIcon />}
-              >
-                <Typography variant="h6" component="h3">
-                  Add account
-                </Typography>
-              </Button>
+    <Card component="section" aria-label="accounts section">
+      <CardHeader
+        title={
+          <Typography variant="h5" component="h2">
+            List of accounts
+          </Typography>
+        }
+        action={
+          <IconButton aria-label="configure account">
+            <SettingsIcon />
+          </IconButton>
+        }
+      />
+
+      <CardContent>
+        <Grid container spacing={1}>
+          {accounts.map(account => (
+            <Grid
+              classes={{ item: classes.item }}
+              item
+              xs={6}
+              sm={4}
+              md={2}
+              key={account.id}
+            >
+              <Account account={account} />
             </Grid>
+          ))}
+          <Grid item xs={6} sm={4} md={2}>
+            <Button
+              classes={{ root: classes.root }}
+              variant="outlined"
+              endIcon={<AddCircleIcon />}
+            >
+              <Typography variant="h6" component="h3">
+                Add account
+              </Typography>
+            </Button>
           </Grid>
-        </CardContent>
-      </Card>
-    </section>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 };

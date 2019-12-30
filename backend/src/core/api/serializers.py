a8056087cafb447d41a9c4be620fc8c1280c2ea9
Bui Kiem <buivantrung1105@gmail.com>
2019-12-30 13:20:25 +0700

    [backend] Create 'get-color-list' endpoint

--- backend/src/core/api/serializers.py
+++ backend/src/core/api/serializers.py
@@ -1,9 +1,17 @@
+from abc import ABC
+
 from rest_framework import serializers
 
 from ..models import Account
 
 
 class AccountSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Account
         fields = ("id", "name", "color", "initial_balance")
+
+
+class AccountColorSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=10)
+    value = serializers.CharField(max_length=10)
--- backend/src/core/api/views.py
+++ backend/src/core/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-from .serializers import AccountSerializer
+from .serializers import AccountSerializer, AccountColorSerializer
 from ..models import Account
 
 
@@ -8,3 +11,9 @@ class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
     http_method_names = ["get", "post", "put", "delete", "head", "options"]
+
+    @action(detail=False, methods=["get"], url_path="get-color-list")
+    def get_color_list(self, request: Request):
+        colors = [{"value": color[0], "name": color[1]} for color in Account.ColorChoices.choices]
+        serializer = AccountColorSerializer(colors, many=True)
+        return Response(serializer.data)

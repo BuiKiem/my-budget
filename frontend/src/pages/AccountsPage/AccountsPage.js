b4e4afb256fff78de4940134450004e50e590dcd
Bui Kiem <buivantrung1105@gmail.com>
2020-01-01 14:51:50 +0700

    Layout AccountsPage.js

--- frontend/src/pages/AccountsPage/AccountsPage.js
+++ frontend/src/pages/AccountsPage/AccountsPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, Paper, Card, Hidden } from "@material-ui/core";
 import { Account } from "../../components/accounts/Account/Account";
 import useAxios from "axios-hooks";
 
@@ -11,10 +12,21 @@ export const AccountsPage = () => {
   if (error) return <p>Error...</p>;
 
   return (
-    <div>
-      {data.map(account => (
-        <Account key={account.id} variant="outlined" account={account} />
-      ))}
-    </div>
+    <Paper component="main">
+      <Grid container justify="space-evenly">
+        <Hidden xsDown>
+          <Grid item component="section" sm={4}>
+            <Card>Account Actions</Card>
+          </Grid>
+        </Hidden>
+        <Grid item component="section" xs={12} sm={7}>
+          <Card>
+            {data.map(account => (
+              <Account key={account.id} variant="outlined" account={account} />
+            ))}
+          </Card>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
